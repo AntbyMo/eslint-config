2c936d8e942b44af44828c9bbf49446b178e9a0b
ZhBano <mozbano@163.com>
2022-07-08 08:59:50 +0800

    docs：vue添加注释

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -12,12 +12,22 @@ module.exports = {
   rules: {
     'prettier/prettier': 0,
 
+    // 强制props有默认值
     'vue/require-default-prop': 'off',
+
+    // setup不能解构
     'vue/no-setup-props-destructure': 'off',
+
+    // 组价名不能简短
     'vue/multi-word-component-names': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
+
+    // template 缩进两列
     'vue/html-indent': ['error', 2],
+
+    // script 缩进两列
     'vue/script-indent': ['error', 2, { baseIndent: 1 }],
+
+    // 属性超过3个自动换行，换行后 同一行不能超过1个
     'vue/max-attributes-per-line': [
       'error',
       {
@@ -30,6 +40,7 @@ module.exports = {
       },
     ],
 
+    // 标签右箭头换行
     'vue/html-closing-bracket-newline': [
       'error',
       {
@@ -38,6 +49,7 @@ module.exports = {
       },
     ],
 
+    // 禁止右箭头有空格
     'vue/html-closing-bracket-spacing': [
       'error',
       {
@@ -47,6 +59,7 @@ module.exports = {
       },
     ],
 
+    // 自闭合标签
     'vue/html-self-closing': [
       'error',
       {
@@ -60,21 +73,26 @@ module.exports = {
       },
     ],
 
+    // {{ 这里有空格 }} 
     'vue/mustache-interpolation-spacing': [2, 'always'],
     "vue/no-multi-spaces": ["error", {
       "ignoreProperties": false
     }],
 
+    // 属性周围不允许等号有空格
     "vue/no-spaces-around-equal-signs-in-attribute": ["error"],
 
+    // template格式
     "vue/component-tags-order": ["error", {
       "order": [ [ "script", "template" ], "style" ]
     }],
 
+    // 强制 defineProps defineEmits的顺序
     "vue/define-macros-order": ["error", {
       "order": ["defineProps", "defineEmits"]
     }],
 
+    // class名排序
     'vue/static-class-names-order':2
   },
 }
