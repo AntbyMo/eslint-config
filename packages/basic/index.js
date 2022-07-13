67662b9a6dec4c6a0616825332e89a125c5d43f7
AntzyMo <mozbano@163.com>
2022-07-13 15:23:39 +0800

    feat: 花括号属性强制换行，import 超过6个换行

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -6,9 +6,7 @@ module.exports = {
   },
   ignorePatterns: ['dist'],
   extends: ['standard'],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
+  parserOptions: { ecmaVersion: 'latest' },
   rules: {
     // arrow function omit bracket
     'arrow-parens': ['error', 'as-needed'],
@@ -35,15 +33,41 @@ module.exports = {
     'no-multiple-empty-lines': [2, { max: 1 }],
 
     // 括号前后 >= 3个换行
-    'array-bracket-newline': [2, { multiline: true, minItems: 3 }],
+    'array-bracket-newline': [
+      2,
+      {
+        multiline: true,
+        minItems: 3
+      }
+    ],
 
     // 括号内的元素 >= 3个换行
-    'array-element-newline': [2, { multiline: true, minItems: 3 }],
+    'array-element-newline': [
+      2,
+      {
+        multiline: true,
+        minItems: 3
+      }
+    ],
 
     // 函数括号内参数有一个换行就换行
     'function-paren-newline': [2, 'multiline'],
 
     // 方法链 >= 3个换行
-    'newline-per-chained-call': [2, { ignoreChainWithDepth: 3 }]
+    'newline-per-chained-call': [2, { ignoreChainWithDepth: 3 }],
+
+    // import 超过6个换行
+    'object-curly-newline': [
+      2,
+      {
+        ImportDeclaration: {
+          multiline: true,
+          minProperties: 6
+        }
+      }
+    ],
+
+    // 花括号属性强制换行，
+    'object-property-newline': [2, { allowAllPropertiesOnSameLine: false }]
   }
 }
