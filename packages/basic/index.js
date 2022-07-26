a87efd3c943dd2b213c0ab3b266653429a20aa00
AntzyMo <mozbano@163.com>
2022-07-26 17:21:26 +0800

    fix: add ignorePatterns

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  ignorePatterns: ['dist'],
+  ignorePatterns: ['dist', '*.d.ts', 'node_modules'],
   extends: ['standard'],
   plugins: ['simple-import-sort'],
   parserOptions: { ecmaVersion: 'latest' },
